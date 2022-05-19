a28077f14c81136caa27276e5976a1e7d71cb7fd
Miguel <mikelezamaoro@gmail.com>
2022-05-19 04:19:20 -0500

    Attemp on server.js fix
    Implemented cors

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-require('dotenv').config()
-
-const express = require('express')
+require("dotenv").config()
+const cors = require("cors");
+const express = require("express")
+const mongoose = require("mongoose")
 const app = express()
-const mongoose = require('mongoose')
+const port = process.env.PORT || 3000
 
 mongoose.connect(process.env.DATABASE_URL_CLOUD, {useNewUrlParser: true})
 const db = mongoose.connection
-db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to database'))
+db.on("error", (error) => console.error(error))
+db.once("open", () => console.log("Connected to Database"))
 
+app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
 const weaponsRouter = require('./routes/weapons.js')
 app.use('/weapons', weaponsRouter)
 
-
-
-app.listen(3000, () => console.log('server started'))
+app.listen(port, () => console.log("Server listening..."))
